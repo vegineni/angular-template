8efc6653c7d7c71426e863c69f848a342f69362f
Leonard Allain-Launay <leonard@codingame.com>
2017-06-08 16:54:36 -0700

    Finalize template v1

--- angular-project/run.sh
+++ angular-project/run.sh
@@ -2,6 +2,9 @@
 
 echo "Building your angular project. Please sit tight. ⏳"
 
-ng build
-
-echo "CG> open --static-dir /project/target/dist /index.html"
+if ng build 2>/dev/null ; then
+    echo "Build success! 🤖"
+    echo "CG> open --static-dir /project/target/dist /index.html"
+else
+    echo "Something when wrong 😱 please look at the compilation logs"
+fi
