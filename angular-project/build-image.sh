c1b43245e9efb75d197b7956099d90197c7e838f
Leonard Allain-Launay <leonard@codingame.com>
2017-06-16 10:50:31 -0700

    Up build script

--- angular-project/build-image.sh
+++ angular-project/build-image.sh
@@ -3,7 +3,7 @@
 cd /project/target
 
 apt-get update
-apt-get install -y curl tee
+apt-get install -y apt-transport-https
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
